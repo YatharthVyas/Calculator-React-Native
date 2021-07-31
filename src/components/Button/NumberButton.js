f0d7919103e69f89bca5e774c5c6e61176687929
YatharthVyas <yatharth2000.vyas@gmail.com>
2021-08-01 00:39:08 +0530

    button for numbers input

---
+++ src/components/Button/NumberButton.js
@@ -0,0 +1,37 @@
+import React, {useContext} from 'react';
+import {TouchableHighlight, StyleSheet, Text} from 'react-native';
+import {EquationContext} from '../../context/EquationContext';
+
+// Display Calculator Buttons for each number
+export default function NumberButton(props) {
+	const {equation, setEquation} = useContext(EquationContext);
+	const onNumberClickHander = () =>{
+		setEquation(equation + props.number)
+	}
+
+	return (
+		<TouchableHighlight
+			onPress={onNumberClickHander}
+			color="#333"
+			accessibilityLabel={`Input Button for Nummber ${props.number}`}
+			style={styles.button}
+		>
+			<Text style={styles.buttonText}>{props.number}</Text>
+		</TouchableHighlight>
+	)
+}
+
+const styles = StyleSheet.create({
+  button: {
+    flex: 1,
+    minHeight: 80,
+    paddingTop: 10,
+    backgroundColor: '#aaa',
+    textAlign: 'center',
+    margin: 5,
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontSize: 40
+  }
+});
