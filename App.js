983530de49c7b3f7ee02b35814b1cad7ab380fcc
YatharthVyas <yatharth2000.vyas@gmail.com>
2021-07-31 18:57:06 +0530

    Provide context and add calculator view

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import CalculatorView from './src/views/Calculator';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <CalculatorView/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,9 +14,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#f0f0f0',
+    flexGrow: 1,
   },
 });
