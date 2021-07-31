2ee0ffdcf79c0c9af9c68f8213bf9dd4c6e12cf6
YatharthVyas <yatharth2000.vyas@gmail.com>
2021-08-01 00:40:01 +0530

    improve the layout for display

--- src/components/InputDisplay/InputDisplay.js
+++ src/components/InputDisplay/InputDisplay.js
@@ -1,13 +1,21 @@
 import React, {useContext} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {ScrollView, StyleSheet, View, Text} from 'react-native';
 import {EquationContext} from '../../context/EquationContext';
 
 export default function InputDisplay () {
   const {equation, setEquation} = useContext(EquationContext);
 	return(
-		<View style={styles.container}>
-			<Text style={styles.displayText}>{equation}</Text>
-		</View>
+    <ScrollView
+      style={{ flex: 1 }}
+      horizontal={true}
+      scrollEnabled={true}
+      automaticallyAdjustContentInsets={true}
+      style={styles.container}
+    >
+  		<View>
+  			<Text style={styles.displayText}>{equation}</Text>
+  		</View>
+    </ScrollView>
 	);
 }
 
@@ -15,14 +23,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     fontSize: 20,
-    padding: 10,
+    paddingTop: 100,
+    paddingRight: 20,
+    paddingLeft: 20,
     borderBottomColor: '#333',
     borderBottomWidth: 1,
-    marginBottom: 15,
-    minHeight: 200
+    minHeight: 200,
+    flexDirection: 'row-reverse'
   },
-  headText: {
+  displayText: {
   	fontSize: 40,
     fontFamily: 'Copperplate',
+    alignSelf: 'flex-end',
+    overflowX: 'scroll',
   }
 });
