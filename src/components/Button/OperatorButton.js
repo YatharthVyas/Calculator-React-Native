de863215b0b8b91b44da1d68d4d96c5c4ef931bf
YatharthVyas <yatharth2000.vyas@gmail.com>
2021-08-01 00:39:22 +0530

    button for operators

---
+++ src/components/Button/OperatorButton.js
@@ -0,0 +1,37 @@
+import React, {useContext} from 'react';
+import {TouchableHighlight, StyleSheet, Text} from 'react-native';
+import {EquationContext} from '../../context/EquationContext';
+
+// Display Calculator Buttons for each number
+export default function OperatorButton(props) {
+	const {equation, setEquation} = useContext(EquationContext);
+	const onNumberClickHander = () =>{
+		setEquation(equation + props.operator)
+	}
+
+	return (
+		<TouchableHighlight
+			onPress={onNumberClickHander}
+			color="#333"
+			accessibilityLabel={`Input Button for Operator ${props.operator}`}
+			style={styles.button}
+		>
+			<Text style={styles.buttonText}>{props.operator}</Text>
+		</TouchableHighlight>
+	)
+}
+
+const styles = StyleSheet.create({
+  button: {
+    flex: 1,
+    minHeight: 80,
+    paddingTop: 10,
+    backgroundColor: '#99f',
+    textAlign: 'center',
+    margin: 5,
+    borderRadius: 10,
+  },
+  buttonText: {
+    fontSize: 40
+  }
+});
